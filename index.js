c58722ad10692207012724daffe01787da5be581
aibek <macbook@Macbooks-MBP.home>
2019-10-29 20:09:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var driver = {
+    name: "Sam",
+    address: "12 Broadway"
+}
+
+// const address = {'11 Broadway'}
+
+function updateDriverWithKeyAndValue(driver,key,value){
+    return Object.assign({}, driver, { [key]: value });
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+ 
+  return driver;  
+
+}
+function deleteFromDriverByKey(driver, key){
+   let newObject = Object.assign({},driver)
+     delete newObject[key]
+        return newObject
+}
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
